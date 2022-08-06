4a23508a5bacfd7cbd09130afd556c132f533da6
Jahiker <rojasjahiker@gmail.com>
2022-08-06 14:40:04 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import Home from "@/components/Home.vue";
+
+export default {
+  components: {
+    Home,
+  }
+}
+</script>
+
+<template>
+  <Home />
+</template>
+
+<style scoped>
+
+</style>
