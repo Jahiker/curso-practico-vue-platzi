b8db17419152afc3111792d4e54ee819c524255c
Jahiker <rojasjahiker@gmail.com>
2022-08-09 22:37:42 -0400

    movements

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,10 +9,15 @@
                 :label-date="labelDate"
                 :amount="amount"
                 :total-amount="100000"
-            />
+            >
+                <template #graphic>graphic</template>
+                <template #action>action</template>
+            </Resume>
         </template>
         <template #movements>
-            <Movements />
+            <Movements 
+                :movements="movements"
+            />
         </template>
     </layout>
 </template>
@@ -33,7 +38,48 @@ export default {
     data() {
         return {
             amount: null,
-            labelDate: null
+            labelDate: null,
+            movements: [{
+                id: 0,
+                title: "Movimiento 1",
+                description: "Lorem ipsum dolor sit amet",
+                amount: 1000,
+            }, {
+                id: 1,
+                title: "Movimiento 2",
+                description: "Lorem ipsum dolor sit amet",
+                amount: 1000,
+            }, {
+                id: 2,
+                title: "Movimiento 3",
+                description: "Lorem ipsum dolor sit amet",
+                amount: 1000,
+            }, {
+                id: 3,
+                title: "Movimiento 4",
+                description: "Lorem ipsum dolor sit amet",
+                amount: 1000,
+            }, {
+                id: 4,
+                title: "Movimiento 5",
+                description: "Lorem ipsum dolor sit amet",
+                amount: 1000,
+            }, {
+                id: 6,
+                title: "Movimiento 7",
+                description: "Lorem ipsum dolor sit amet",
+                amount: 1000,
+            }, {
+                id: 7,
+                title: "Movimiento 8",
+                description: "Lorem ipsum dolor sit amet",
+                amount: 1000,
+            }, {
+                id: 8,
+                title: "Movimiento 9",
+                description: "Lorem ipsum dolor sit amet",
+                amount: 1000,
+            }],
         }
     }
 }
---
+++ src/components/MovementItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="movement">
+        <span>{{ title }}</span>
+    </div>
+</template>
+
+<script setup>
+import { defineProps, toRefs } from 'vue';
+
+const props = defineProps({
+    title: {
+        type: String,
+    }
+});
+
+const { title } = toRefs(props);
+
+</script>
