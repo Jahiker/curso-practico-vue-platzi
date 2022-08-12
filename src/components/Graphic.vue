e0d98c540b39a8fae05e8ebf39b0bcd5b5e0be78
Jahiker <rojasjahiker@gmail.com>
2022-08-12 00:04:30 -0400

    [add] graphic component

---
+++ src/components/Graphic.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="graphic">
+        <svg
+            viewBox="0 0 300 200"
+        >
+            <line 
+                stroke="#c4c4c4"
+                stroke-width="2"
+                x1="0"
+                :y1="zero"
+                x2="300"
+                :y2="zero"
+            />
+            <polyline 
+                fill="none"
+                stroke="#0689b0"
+                stroke-width="2"
+                :points="points"
+            />
+            <line 
+                stroke="#04b500"
+                stroke-width="2"
+                x1="200"
+                y1="0"
+                x2="200"
+                y2="200"
+            />
+        </svg>
+        <p>Ultimos 30 dias</p>
+    </div>
+</template>
+
+<script setup>
+    import { defineProps, toRefs, computed } from 'vue';
+
+    const props = defineProps({
+        amounts: {
+            type: Array,
+            default: () => []
+        }
+    })
+
+    const { amounts } = toRefs(props);
+
+    const amountToPixels = (amount) => {
+        const min = Math.min(...amounts.value);
+        const max = Math.max(...amounts.value);
+
+        const amountAbs = amount + Math.abs(min);
+        const minmax = Math.abs(max) + Math.abs(min);
+
+        return 200 - ((amountAbs * 100) / minmax) * 2;
+    }
+
+    const zero = computed(() => {
+        return amountToPixels(0);
+    })
+
+    const points = computed(() => {
+        const total = amounts.value.length;
+        return amounts.value.reduce((points, amount, i) => {
+            const x = (300 / total) * (i + 1);
+            const y = amountToPixels(amount);
+            return `${points} ${x},${y}`;
+        }, "0,100")
+    })
+</script>
+
+<style scoped>
+svg {
+  width: 100%;
+}
+p {
+  text-align: center;
+  margin: 20px auto;
+}
+</style>
