c0a569e5ba80c81fd253815be828dc1dfd9c2728
Jahiker <rojasjahiker@gmail.com>
2022-08-13 23:40:04 -0400

    touch events

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -45,42 +45,52 @@ export default {
                 id: 0,
                 title: "Movimiento 1",
                 description: "Lorem ipsum dolor sit amet",
-                amount: 1000,
+                amount: 100,
             }, {
                 id: 1,
                 title: "Movimiento 2",
                 description: "Lorem ipsum dolor sit amet",
-                amount: -1000,
+                amount: 200,
             }, {
                 id: 2,
                 title: "Movimiento 3",
                 description: "Lorem ipsum dolor sit amet",
-                amount: 1000,
+                amount: 500,
             }, {
                 id: 3,
                 title: "Movimiento 4",
                 description: "Lorem ipsum dolor sit amet",
-                amount: 1000,
+                amount: 200,
             }, {
                 id: 4,
                 title: "Movimiento 5",
                 description: "Lorem ipsum dolor sit amet",
-                amount: -1000,
+                amount: -400,
             }, {
                 id: 6,
                 title: "Movimiento 7",
                 description: "Lorem ipsum dolor sit amet",
-                amount: -1000,
+                amount: -600,
             }, {
                 id: 7,
                 title: "Movimiento 8",
                 description: "Lorem ipsum dolor sit amet",
-                amount: 1000,
+                amount: -300,
             }, {
                 id: 8,
                 title: "Movimiento 9",
                 description: "Lorem ipsum dolor sit amet",
-                amount: 1000,
+                amount: 0,
+            }, {
+                id: 9,
+                title: "Movimiento 10",
+                description: "Lorem ipsum dolor sit amet",
+                amount: 300,
+            }, {
+                id: 10,
+                title: "Movimiento 11",
+                description: "Lorem ipsum dolor sit amet",
+                amount: 500,
             }],
         }
     }
